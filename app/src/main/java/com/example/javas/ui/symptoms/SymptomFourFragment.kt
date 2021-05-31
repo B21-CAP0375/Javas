ad16e857ceac35ecc8ea35bf8d1af75f8d4cf531
MIrsyadRafiSudirjo <irsyadicad1302@gmail.com>
2021-05-31 10:48:45 +0700

    add tflite process

--- app/src/main/java/com/example/javas/ui/symptoms/SymptomFourFragment.kt
+++ app/src/main/java/com/example/javas/ui/symptoms/SymptomFourFragment.kt
@@ -7,8 +7,16 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import com.example.javas.R
 import com.example.javas.databinding.FragmentSymptomFourBinding
+import com.example.javas.ml.Javas
+import com.example.javas.ml.Javasfix
+import com.example.javas.ml.Javasmodel
+import com.example.javas.ml.Javasmodelsymptom
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.common.TensorProcessor
+import org.tensorflow.lite.support.common.ops.NormalizeOp
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.nio.ByteBuffer
 
 
 class SymptomFourFragment : Fragment() {
@@ -48,6 +56,7 @@ class SymptomFourFragment : Fragment() {
         val memakaiMasker = SymptomFourFragmentArgs.fromBundle(arguments as Bundle).memakaiMasker
         val menggunakanSanitizer = SymptomFourFragmentArgs.fromBundle(arguments as Bundle).menggunakanSanitizer
 
+
         if (backPressed){
             if (mengikutiAcara) {
                 binding.rbYes1.isChecked = true
@@ -80,11 +89,108 @@ class SymptomFourFragment : Fragment() {
             }
         }
 
+
+
         binding.btnNextResult.setOnClickListener {
-            val name = SymptomFourFragmentArgs.fromBundle(arguments as Bundle).name
-            val toResult = SymptomFourFragmentDirections.actionSymptomFourFragmentToChooseDateVaccineFragment()
-            toResult.name=name
-            view.findNavController().navigate(toResult)
+//            val model = context?.let { it1 -> Javasmodel.newInstance(it1) }
+//
+//            val modela = context?.let { it1 -> Javas.newInstance(it1) }
+
+//            val modela = context?.let { it1 -> Javasmodelsymptom.newInstance(it1) }
+
+            val model = context?.let { it1 -> Javasfix.newInstance(it1) }
+
+
+            fun demam(): Float = if (demam) 1f else 0f
+            fun sakitPernapasan(): Float = if (sakitPernapasan) 1f else 0f
+            fun batukKering(): Float = if (batukKering) 1f else 0f
+            fun sakitTenggorokan(): Float = if (sakitTenggorokan) 1f else 0f
+            fun flu(): Float = if (flu) 1f else 0f
+            fun asma(): Float = if (asma) 1f else 0f
+            fun penyakitParu(): Float = if (penyakitParu) 1f else 0f
+            fun sakitKepala(): Float = if (sakitKepala) 1f else 0f
+            fun sakitJantung(): Float = if (sakitJantung) 1f else 0f
+            fun diabetes(): Float = if (diabetes) 1f else 0f
+            fun hipertensi(): Float = if (hiperTensi) 1f else 0f
+            fun kecapean(): Float = if (kecapean) 1f else 0f
+            fun gangguanPencernaan(): Float = if (gangguanPencernaan) 1f else 0f
+            fun baruBepergian(): Float = if (baruBepergian) 1f else 0f
+            fun bertemuPasienCovid(): Float = if (bertemuPasienCovid) 1f else 0f
+            fun mengikutiAcara(): Float = if (mengikutiAcara) 1f else 0f
+            fun bertemuOrangBanyak(): Float = if (bertemuOrangBanyak) 1f else 0f
+            fun anggotaKeluargaBekerja(): Float = if (anggotaKeluargaBekerja) 1f else 0f
+
+            val byteBuffer : ByteBuffer = ByteBuffer.allocateDirect(18*4)
+            byteBuffer.putFloat(demam())
+            byteBuffer.putFloat(sakitPernapasan())
+            byteBuffer.putFloat(batukKering())
+            byteBuffer.putFloat(sakitTenggorokan())
+            byteBuffer.putFloat(flu())
+            byteBuffer.putFloat(asma())
+            byteBuffer.putFloat(penyakitParu())
+            byteBuffer.putFloat(sakitKepala())
+            byteBuffer.putFloat(sakitJantung())
+            byteBuffer.putFloat(diabetes())
+            byteBuffer.putFloat(hipertensi())
+            byteBuffer.putFloat(kecapean())
+            byteBuffer.putFloat(gangguanPencernaan())
+            byteBuffer.putFloat(baruBepergian())
+            byteBuffer.putFloat(bertemuPasienCovid())
+            byteBuffer.putFloat(mengikutiAcara())
+            byteBuffer.putFloat(bertemuOrangBanyak())
+            byteBuffer.putFloat(anggotaKeluargaBekerja())
+
+
+
+
+
+            val array = byteArrayOf(4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4)
+//            demam(), sakitPernapasan(), batukKering(),sakitTenggorokan()
+//            ,flu(),asma(),penyakitParu(),sakitKepala(),sakitJantung(),diabetes()
+//            ,hipertensi(),kecapean(),gangguanPencernaan(),baruBepergian(),bertemuPasienCovid(),mengikutiAcara(),bertemuOrangBanyak()
+//            ,anggotaKeluargaBekerja()
+              //val byteBuffer = ByteBuffer.wrap(array)
+
+            val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 18), DataType.FLOAT32)
+            inputFeature0.loadBuffer(byteBuffer)
+
+            val outputs = model?.process(inputFeature0)
+            val outputFeature0 = outputs?.outputFeature0AsTensorBuffer
+
+            val result = outputFeature0?.floatArray?.get(0)
+            if (result != null) {
+                Toast.makeText(context, result.toString(), Toast.LENGTH_SHORT).show()
+//                if (result < 0.5) {
+//                    Toast.makeText(context, "mantap", Toast.LENGTH_SHORT).show()
+//                } else {
+//                    Toast.makeText(context, "koplo", Toast.LENGTH_SHORT).show()
+//                }
+            }
+//            val tensorLabel =
+//                outputFeature0?.let { it1 ->
+//                    TensorLabel(
+//                        arrayListOf("yes", "no"),
+//                        it1
+//                    )
+//                }
+//
+//            val probability = tensorLabel?.mapWithFloatValue?.get("yes")
+//            probability?.let {
+//                if (it > 0.80) {
+//                    Toast.makeText(context, "mantappp", Toast.LENGTH_SHORT).show()
+//                } else {
+//                    Toast.makeText(context, "gebleg", Toast.LENGTH_SHORT).show()
+//                }
+//            }
+
+
+//            val name = SymptomFourFragmentArgs.fromBundle(arguments as Bundle).name
+//            val toResult = SymptomFourFragmentDirections.actionSymptomFourFragmentToChooseDateVaccineFragment()
+//            toResult.name=name
+            if (model != null) {
+                model.close()
+            }
+//            view.findNavController().navigate(toResult)
         }
 
     }
